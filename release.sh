feadc70aee5ff7346e90341fca15d68599c54afd
ChongOS <chaiyut.na@gmail.com>
2017-08-16 10:08:17 +0700

    Upload annotations

--- release.sh
+++ release.sh
@@ -5,5 +5,6 @@
 read -p 'bintrayUser: ' bintrayUser
 read -p 'bintrayKey: ' -s bintrayKey
 
+./gradlew :prefman-annotations:bintrayUpload -PbintrayUser=$bintrayUser -PbintrayKey=$bintrayKey -PdryRun=false
 ./gradlew :prefman-compiler:bintrayUpload -PbintrayUser=$bintrayUser -PbintrayKey=$bintrayKey -PdryRun=false
 ./gradlew :prefman:bintrayUpload -PbintrayUser=$bintrayUser -PbintrayKey=$bintrayKey -PdryRun=false
